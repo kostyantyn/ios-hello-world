e9a39e549d72deaece81891d999620f2cb449932
Kostyantyn <kostya.stepanyuk@gmail.com>
2013-01-20 20:16:08 +0200

    Initial commit

---
+++ HelloWorld/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by Kostyantyn on 1/20/13.
+//  Copyright (c) 2013 Kostyantyn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HelloWorld/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HelloWorld
+//
+//  Created by Kostyantyn on 1/20/13.
+//  Copyright (c) 2013 Kostyantyn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
